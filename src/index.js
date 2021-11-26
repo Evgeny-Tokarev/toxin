25cecc70ef1fe2bcaefa33ed96c630eed21470c6
Evgeny <ghenya.t@gmail.com>
2021-11-26 19:35:17 +0300

    Webpack configuration in more succesful way

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-import "./js/common.js"
+import "./js/common.js";
+import './style/style.css';
+import './img/kaka.png'
+let add = (a,b) => a+b;
+console.log(add(4,20));
+
+
+function component() {
+    const h1 = document.createElement("h1")
+  
+    h1.innerHTML =("Webpack is kakashka!!!");
+  
+    return h1;
+  }
+  
+  document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,24 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
+    mode: 'development',    
     entry: {
         app: "./src/index.js"
     },
-
     devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+      },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Development'
+        }),
+      ],
     output: {
         filename: "[name].js",
-        path: path.resolve(__dirname,"../dist"),
-        publicPath: "/dist"
+        path: path.resolve(__dirname,"dist"),
+        clean: true,
     },
     
     module: {
@@ -16,11 +26,19 @@ module.exports = {
             test: /\.js$/,
             loader: "babel-loader",
             exclude: "/node-modules/"
-        }]
-    },
-    devServer: {
-        client: {
-          overlay: true,
         },
-      },
+        {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          },
+          {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+          },
+          {
+            test: /\.(woff|woff2|eot|ttf|otf)$/i,
+            type: 'asset/resource',
+          },
+        ]
+    },
 }
