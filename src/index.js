1d46fd360f6469c620a54bc11afa654837f7e0f5
Evgeniy <ghenya.t@gmail.com>
2021-11-27 23:49:17 +0300

    Progressing with webpack configurating

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import "./js/common.js";
-import './style/style.css';
-import './img/kaka.png'
-let add = (a,b) => a+b;
-console.log(add(4,20));
-
+import "./style/style.css";
+import "./img/kaka.png";
+let adddddddddd = (aaa, b) => aaa + b;
+console.log(adddddddddd(4, 999999));
 
 function component() {
-    const h1 = document.createElement("h1")
-  
-    h1.innerHTML =("Webpack is kakashka!!!");
-  
-    return h1;
-  }
-  
-  document.body.appendChild(component());
+  const h1 = document.createElement("h1");
+
+  h1.innerHTML = "Webpack is kakashka!!!";
+
+  return h1;
+}
+
+document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,57 @@
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    mode: 'development',    
-    entry: {
-        app: "./src/index.js"
+  mode: "development",
+  entry: {
+    app: "./src/index.js",
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
+
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "all",
     },
-    devtool: 'inline-source-map',
-    devServer: {
-        static: './dist',
-      },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Development'
-        }),
-      ],
-    output: {
-        filename: "[name].js",
-        path: path.resolve(__dirname,"dist"),
-        clean: true,
-    },
-    
-    module: {
-        rules: [{
-            test: /\.js$/,
-            loader: "babel-loader",
-            exclude: "/node-modules/"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
         },
-        {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-          },
-          {
-            test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            type: 'asset/resource',
-          },
-          {
-            test: /\.(woff|woff2|eot|ttf|otf)$/i,
-            type: 'asset/resource',
-          },
-        ]
-    },
-}
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Development",
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+};
