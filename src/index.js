8e9a611a8d251eb777ebec5869c98e66540efe77
Evgeny <ghenya.t@gmail.com>
2021-12-03 19:56:05 +0300

    Stuck in recursive choosing for pug files

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 // import * as $ from "jquery";
-import "./style/style.scss";
-import "./img/kaka.png";
+import './style/style.scss';
+import './img/kaka.png';
 function component() {
-  const h1 = document.createElement("h1");
+  const h1 = document.createElement('h1');
 
-  h1.innerHTML = "Webpack";
-  h1.classList.add("heading");
+  h1.innerHTML = 'Webpack';
+  h1.classList.add('heading');
 
   return h1;
 }
--- webpack.common.js
+++ webpack.common.js
@@ -1,65 +1,83 @@
-const path = require("path");
-const fs = require("fs");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const ESLintPlugin = require("eslint-webpack-plugin");
+const path = require('path');
+const fs = require('fs');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
-const isDev = process.env.NODE_ENV === "development";
+const isDev = process.env.NODE_ENV === 'development';
 const PATHS = {
-  src: path.join(__dirname, "/src"),
-  dist: path.join(__dirname, "/dist"),
-};
-const isDir = (dirPath) => {
-  return fs.existsSync(dirPath) && fs.lstatSync(dirPath).isDirectory();
+  src: path.join(__dirname, '/src'),
+  dist: path.join(__dirname, '/dist'),
 };
 
-const PAGES_DIR = `${PATHS.src}/pages/`;
+const PAGES_DIR = `${PATHS.src}/pages`;
 
-const PAGES = fs
-  .readdirSync(PAGES_DIR)
-  .map((dirName) => {
-    return path.join(PAGES_DIR, dirName);
-  })
-  .filter(isDir);
+const getPugFiles = (dir) => {
+  console.log(dir)
+  const commonPartLength= dir.split('\/').length;
+  const res = [];
+  const iteration = (currentDir) => {
+    console.log(currentDir)
+    const files = fs.readdirSync(currentDir) 
+    console.log(files);
+    for (let file of files){
+       const status = fs.statSync(`${currentDir}/${file}`);
+          if(status.isDirectory()){
+             iteration(`${currentDir}/${file}`); 
+          }else{
+            if (file.endsWith('.pug')){
+              console.log(`file ${file}`);
+              console.log('current dir 2 ' + currentDir);
+              console.log(`что мы запишем ${currentDir.split('\/').slice(commonPartLength)}/${file}`);
+            res.push(`${currentDir.split('\/').slice(commonPartLength)}/${file}`);
+            }
+          }
+    }
+  }
+  iteration(dir);
+  return res;
+}
+const PAGES = getPugFiles(PAGES_DIR);
 console.log(PAGES);
-// const pugs = PAGES.map(page => {
-//   fs.readdirSync(page).filter(fileName => fileName.endsWith('.pug'))
-// }
 
 module.exports = {
-  context: path.resolve(__dirname, "src"),
+  context: path.resolve(__dirname, 'src'),
   entry: {
-    app: "./index.js",
+    app: './index.js',
   },
   optimization: {
     splitChunks: {
-      chunks: "all",
+      chunks: 'all',
     },
     minimize: !isDev,
     minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./index.html",
+      template: './index.html',
       minify: !isDev,
     }),
+    ...PAGES.map(page => new HtmlWebpackPlugin({
+      template: `${PAGES_DIR}/${page}`,
+      filename: `./${page.replace(/\.pug/,'.html')}`
+    })),
     new MiniCssExtractPlugin({
-      filename: "[name].[contenthash].css",
+      filename: '[name].[contenthash].css',
     }),
     new CopyPlugin({
-      patterns: [{ from: "./img/icons8-webpack-64.png", to: "../dist" }],
+      patterns: [{ from: './img/icons8-webpack-64.png', to: '../dist' }],
     }),
     new TerserPlugin(),
     new ESLintPlugin({
-      extensions: ["js"],
+      extensions: ['js'],
     }),
   ],
   output: {
-    filename: "[name].[contenthash].js",
-    path: path.resolve(__dirname, "dist"),
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
     charset: true,
   },
@@ -68,32 +86,36 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        exclude: "/node-modules/",
+        exclude: '/node-modules/',
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
             sourceMap: true,
           },
         },
       },
+      // {
+      //   test: /\.css$/i,
+      //   use: [MiniCssExtractPlugin.loader, "css-loader"],
+      // },
       {
         test: /\.s[ac]ss$/i,
         use: [
-          isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+          isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
           {
-            loader: "css-loader",
+            loader: 'css-loader',
             options: {
               sourceMap: true,
             },
           },
           {
-            loader: "postcss-loader",
+            loader: 'postcss-loader',
             options: {
               sourceMap: true,
             },
           },
           {
-            loader: "sass-loader",
+            loader: 'sass-loader',
             options: {
               sourceMap: true,
             },
@@ -102,11 +124,11 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
     ],
   },
