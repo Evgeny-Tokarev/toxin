2180461d0f0e910ccb1a68e611bf8523a67e43a9
Evgeny <ghenya.t@gmail.com>
2021-12-13 18:00:58 +0300

    dropdown logic first iteration added

--- src/pages/ui-kit/formElements/dropdown/dropdown.js
+++ src/pages/ui-kit/formElements/dropdown/dropdown.js
@@ -3,12 +3,22 @@ const list = $('.select-list')
 const decrButtons = $('.select-list__button_type_decrease')
 const incrButtons = $('.select-list__button_type_increase')
 const listValues = $('.select-list__item-value')
+const valueArr = [];
 
-$('.input__field').on('click', function () {
-    $('.select-list__item').toggleClass('select-list__item_hidden')
-    $(this).addClass('input__field_expanded')
-})
-list.on('click', (event) => {
+// При клике на 'input__button' добавляем модифицированный класс 'input__body_expanded' для '.input__body'
+
+$(document).on('click', '.input__body', function(e) {
+    if (($(e.target).hasClass('input__button'))) { 
+        e.stopPropagation()
+        $(this).toggleClass('input__body_expanded')
+    }
+});
+
+// Логика работы кнопок + и - в пунктах меню
+
+list.on('click', function(event) {
+    const placeholder =$(this).closest('.input__body').find('.input__placeholder');
+    const context = this
     decrButtons.each((i, button) => {
         if (event.target === button) {
             if (listValues[i].innerHTML > 1) {
@@ -33,4 +43,19 @@ list.on('click', (event) => {
             }
         }
     })
+    // Записываем выбранные значения в тело поля ввода
+
+listValues.each(function(i,value) {
+    if ((value.innerHTML > 0)&&(context.contains(value))) {
+        valueArr[i]= `${$(value).closest('.select-list__item').find('.select-list__name').html()} ${value.innerHTML}`
+    } else {
+        if (context.contains(value)) {
+        valueArr[i]=null;
+        }
+    }
 })
+placeholder.text(valueArr.filter(Boolean).length ? valueArr.filter(Boolean).join(', '): $(this).closest('.input__body').find('.input__placeholder').attr('data-value'))  
+})
+
+
+
