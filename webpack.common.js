06907f70c6a9da3c8f2bdc7edd16a03fcac81833
Evgeniy <ghenya.t@gmail.com>
2021-12-20 23:11:57 +0300

    Material-icon font embeded

--- webpack.common.js
+++ webpack.common.js
@@ -1,130 +1,136 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const ESLintPlugin = require('eslint-webpack-plugin');
-const glob = require('glob');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const ESLintPlugin = require('eslint-webpack-plugin')
+const glob = require('glob')
 
-const isDev = process.env.NODE_ENV === 'development';
+const isDev = process.env.NODE_ENV === 'development'
 const PATHS = {
-  src: path.join(__dirname, '/src'),
-  dist: path.join(__dirname, '/dist'),
-};
+    src: path.join(__dirname, '/src'),
+    dist: path.join(__dirname, '/dist'),
+}
 
-const PAGES_DIR = `${PATHS.src}/pages`;
+const PAGES_DIR = `${PATHS.src}/pages`
 
-const PAGES = glob.sync(`${PAGES_DIR}/**/*.pug`);
-console.log(PAGES);
+const PAGES = glob.sync(`${PAGES_DIR}/**/*.pug`)
+console.log(PAGES)
 
 module.exports = {
-  target: 'web',
-  context: path.resolve(__dirname, 'src'),
-  entry: {
-    app: './index.js',
-  },
-  devServer: {
-    liveReload: true,
-    static: [
-      {
-        directory: path.join(__dirname, '../dist'),
-      },]
-  },
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
+    target: 'web',
+    context: path.resolve(__dirname, 'src'),
+    entry: {
+        app: './index.js',
     },
-    minimize: !isDev,
-    minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.pug',
-      filename: 'index.html',
-      minify: !isDev,
-    }),
-    ...PAGES.map(
-      (page) =>
-        new HtmlWebpackPlugin({
-          template: page,
-          filename: `[contenthash]${page
-            .split('/')
-            .slice(-1)
-            .join('/')
-            .replace(/\.pug/, '.html')}`,
-        })
-    ),
-    new MiniCssExtractPlugin({
-      filename: '[name].[contenthash].css',
-    }),
-    new CopyPlugin({
-      patterns: [{ from: './img/icons8-webpack-64.png', to: '../dist' },
-      {from: './plugins/jquery-ui/images/*.*', to: '../dist/images/*.*'}],
-    }),
-    new TerserPlugin(),
-    new ESLintPlugin({
-      extensions: ['js'],
-    }),
-  ],
-  output: {
-    filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-    charset: true,
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: '/node-modules/',
-        use: {
-          loader: 'babel-loader',
-          options: {
-            sourceMap: true,
-          },
-        },
-      },
-      {
-        test: /\.pug$/,
-        use: ['pug-loader'],
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true,
+    devServer: {
+        liveReload: true,
+        static: [
+            {
+                directory: path.join(__dirname, '../dist'),
             },
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              sourceMap: true,
-            },
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: true,
-            },
-          },
         ],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: '[name][ext]',
-        }
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf|svg)$/i,
-        type: 'asset/resource',
-      },
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+        },
+        minimize: !isDev,
+        minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.pug',
+            filename: 'index.html',
+            minify: !isDev,
+        }),
+        ...PAGES.map(
+            (page) =>
+                new HtmlWebpackPlugin({
+                    template: page,
+                    filename: `[contenthash]${page
+                        .split('/')
+                        .slice(-1)
+                        .join('/')
+                        .replace(/\.pug/, '.html')}`,
+                })
+        ),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: './img/icons8-webpack-64.png', to: '../dist' },
+                {
+                    from: './plugins/jquery-ui/images/*.*',
+                    to: '../dist/images',
+                },
+            ],
+        }),
+        new TerserPlugin(),
+        new ESLintPlugin({
+            extensions: ['js'],
+        }),
     ],
-  },
-};
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+        charset: true,
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: '/node-modules/',
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        sourceMap: true,
+                    },
+                },
+            },
+            {
+                test: /\.pug$/,
+                use: ['pug-loader'],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: '[name][ext]',
+                },
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf|svg)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+}
