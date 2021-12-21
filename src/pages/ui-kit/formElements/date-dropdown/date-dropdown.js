4563fd47f0b0aedcb4047d22ea896130141f5a7f
Evgeny <ghenya.t@gmail.com>
2021-12-21 19:03:20 +0300

    Jq widget factory embeding in process

--- src/pages/ui-kit/formElements/date-dropdown/date-dropdown.js
+++ src/pages/ui-kit/formElements/date-dropdown/date-dropdown.js
@@ -1,6 +1,18 @@
 import * as $ from 'jquery'
+import '../../../../plugins/jquery-ui/jquery-ui.js'
 
+
+
+console.log($.ui)
 console.log($.fn)
-$( function() {
-    $( '#datepicker' ).datepicker();
-  } )
+// $( function() {
+//     $( '#datepicker' ).datepicker();
+//   } )
+
+// $('span .ui-icon-circle-triangle-w .ui-corner-all').text('arrow_back')
+
+$.widget( 'toxin.daepicker', $.ui.datepicker, {
+  options: {
+
+  },
+});
