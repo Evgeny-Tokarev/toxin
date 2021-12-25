ab1ad5a07b236d279d8aab01122645e3a41ec078
Evgeniy <ghenya.t@gmail.com>
2021-12-26 00:44:14 +0300

    Air-datepicker styling ready

--- src/pages/ui-kit/formElements/date-dropdown/date-dropdown.js
+++ src/pages/ui-kit/formElements/date-dropdown/date-dropdown.js
@@ -1,26 +1,44 @@
-// import * as $ from 'jquery'
-import AirDatepicker from 'air-datepicker'
-
+import * as $ from 'jquery';
+import AirDatepicker from 'air-datepicker';
 const submitButton = {
     content: 'Применить',
     className: 'air-datepicker--submitButton',
     onClick: (dp) => {
-        console.log(dp.selectedDates)
-
-    }
-}
-
-new AirDatepicker('#datepicker', {inline: true,
-
-    buttons: [  'clear',submitButton],
-    startDate: [new Date(2019, 7, 8)],
-    navTitles: {
-        days(dp) {
-            return `<span>
+        console.log(dp.selectedDates);
+        dp.hide();
+        dp.clear();
+    },
+};
+const dp = [];
+$('.datepicker').each(function (i) {
+    dp[i] = new AirDatepicker('.datepicker', {
+        buttons: ['clear', submitButton],
+        startDate: [new Date(2019, 7, 8)],
+        navTitles: {
+            days(dp) {
+                return `<span>
                      ${dp.formatDate(dp.selectedDates[0], 'MMMM yyyy')}
                 </span>`;
+            },
+        },
+        // showEvent: '',
+        prevHtml: `<span>arrow_back</span>`,
+        nextHtml: `<span>arrow_forward</span>`,
+    });
+    console.log(dp[i]);
+    const button = $(this).closest('.input__body').find('.input__button');
+    const input = this;
+    button.on('click', function () {
+        console.log($(dp[i].$datepicker).hasClass('-active-'));
+        if ($(dp[i].$datepicker).hasClass('-active-')) {
+            $(input).trigger('blur');
+            console.log('-');
+            // dp[i].hide();
+        } else {
+            $(input).trigger('focus');
+            console.log('+');
+
+            // dp[i].show();
         }
-    },
-    prevHtml: `<span>arrow_back</span>`,
-    nextHtml: `<span>arrow_forward</span>`,
-})
+    });
+});
