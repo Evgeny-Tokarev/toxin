63e04fdcaa0f51e9276e08780a76609f3b85b5b3
Evgeny <ghenya.t@gmail.com>
2021-12-25 13:09:52 +0300

    Date dropdown decorating from air datepicker

--- src/pages/ui-kit/formElements/date-dropdown/date-dropdown.js
+++ src/pages/ui-kit/formElements/date-dropdown/date-dropdown.js
@@ -1,9 +1,26 @@
 // import * as $ from 'jquery'
 import AirDatepicker from 'air-datepicker'
 
-new AirDatepicker('#datepicker', {
-    inline: true,
+const submitButton = {
+    content: 'Применить',
+    className: 'air-datepicker--submitButton',
+    onClick: (dp) => {
+        console.log(dp.selectedDates)
 
+    }
+}
+
+new AirDatepicker('#datepicker', {inline: true,
+
+    buttons: [  'clear',submitButton],
+    startDate: [new Date(2019, 7, 8)],
+    navTitles: {
+        days(dp) {
+            return `<span>
+                     ${dp.formatDate(dp.selectedDates[0], 'MMMM yyyy')}
+                </span>`;
+        }
+    },
     prevHtml: `<span>arrow_back</span>`,
     nextHtml: `<span>arrow_forward</span>`,
 })
