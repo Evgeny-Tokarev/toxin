a4e5782af7433d9160493d2fbd2d8637bc445b1d
Evgeniy <ghenya.t@gmail.com>
2022-01-06 00:07:08 +0300

    toggle ready

--- src/pages/ui-kit/formElements/expandable-checkbox/expandable-checkbox.js
+++ src/pages/ui-kit/formElements/expandable-checkbox/expandable-checkbox.js
@@ -1,15 +1,23 @@
 import Arrow_button from '../blanks/arrow-button/arrow-button';
+const values = [];
 
-const button = new Arrow_button();
-button.init($('.input_type_ecb').find('.input__body'));
-const values = {};
-$('.input_type_ecb input').on('change', function () {
-    $.each($('.input_type_ecb .input__checkbox-item'), function () {
-        if ($(this).find('input').prop('checked')) {
-            values[$(this).text()] = true;
-        } else {
-            delete values[$(this).text()];
-        }
+$('.input_type_ecb')
+    .find('.input__body')
+    .each(function (i) {
+        const button = new Arrow_button();
+        const self = this;
+        values[i] = {};
+        button.init(this);
+        $(this)
+            .find('input')
+            .on('change', function () {
+                $.each($(self).find('.input__checkbox-item'), function () {
+                    if ($(this).find('input').prop('checked')) {
+                        values[i][$(this).text()] = true;
+                    } else {
+                        delete values[i][$(this).text()];
+                    }
+                });
+                console.log(Object.getOwnPropertyNames(values[i]));
+            });
     });
-    console.log(Object.getOwnPropertyNames(values));
-});
--- src/pages/ui-kit/formElements/radio-buttons/radio-buttons.js
+++ src/pages/ui-kit/formElements/radio-buttons/radio-buttons.js
@@ -1,11 +1,17 @@
-const values = {};
-$('.input_type_rb input').on('change', function () {
-    $.each($('.input_type_rb .input__checkbox-item'), function () {
-        if ($(this).find('input').prop('checked')) {
-            values[$(this).text()] = true;
-        } else {
-            delete values[$(this).text()];
-        }
-    });
-    console.log(Object.getOwnPropertyNames(values));
+const values = [];
+$('.input_type_rb').each(function (i) {
+    const self = this;
+    values[i] = {};
+    $(this)
+        .find('input')
+        .on('change', function () {
+            $.each($(self).find('.input__checkbox-item'), function () {
+                if ($(this).find('input').prop('checked')) {
+                    values[i][$(this).text()] = true;
+                } else {
+                    delete values[i][$(this).text()];
+                }
+            });
+            console.log(Object.getOwnPropertyNames(values[i]));
+        });
 });
