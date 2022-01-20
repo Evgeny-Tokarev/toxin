c5e80cee2571b2aa31c05992bfee7cd9feca2b43
Evgeny <ghenya.t@gmail.com>
2022-01-20 16:34:29 +0300

    Murad issue

--- src/pages/ui-kit/formElements/radio-buttons/radio-buttons.js
+++ src/pages/ui-kit/formElements/radio-buttons/radio-buttons.js
@@ -2,20 +2,3 @@ import Checkbox from '../blanks/checkbox/checkbox';
 
 const radioButtons = new Checkbox();
 radioButtons.init('.input_type_rb');
-// const values = [];
-// $('.input_type_rb').each(function (i) {
-//     const self = this;
-//     values[i] = {};
-//     $(this)
-//         .find('input')
-//         .on('change', function () {
-//             $.each($(self).find('.input__checkbox-item'), function () {
-//                 if ($(this).find('input').prop('checked')) {
-//                     values[i][$(this).text()] = true;
-//                 } else {
-//                     delete values[i][$(this).text()];
-//                 }
-//             });
-//             console.log(Object.getOwnPropertyNames(values[i]));
-//         });
-// });
